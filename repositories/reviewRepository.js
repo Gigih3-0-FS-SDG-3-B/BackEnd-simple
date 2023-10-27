286e3c4cd1ca438762ef0605523b6025615bd9b0
FelixIG15 <felix.ganumba@gmail.com>
2023-10-28 01:29:43 +0700

    hotfix(repositories): add review period

--- repositories/reviewRepository.js
+++ repositories/reviewRepository.js
@@ -1,6 +1,20 @@
 import { withPrisma } from "../middlewares/prismaMiddleware.js";
+import { getOrder } from "./orderRepository.js";
+import { REVIEW_PERIOD_DAYS_AFTER_CHECKOUT } from "../enums/reviewEnum.js";
 
 export async function createReview(orderId, review, reviewRating) {
+  const order = await getOrder(orderId);
+
+  const today = new Date();
+  const sevenDaysAfterEnd = new Date(order.date_end);
+  sevenDaysAfterEnd.setDate(
+    sevenDaysAfterEnd.getDate() + REVIEW_PERIOD_DAYS_AFTER_CHECKOUT
+  );
+
+  if (today > sevenDaysAfterEnd) {
+    throw new Error("Review Period has passed");
+  }
+
   return await withPrisma(async (prisma) => {
     return prisma.reviews.create({
       data: {
