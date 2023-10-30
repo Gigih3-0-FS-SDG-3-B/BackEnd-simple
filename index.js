d5c9992ecc0601361cd3430535706cea5e907f42
wimarar <wimarabdurrachman26@gmail.com>
2023-10-30 16:26:10 +0800

    revision jwt

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import router from "./routes.js";
 
 const app = express();
 const port = 3000;
@@ -10,66 +9,8 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Define the same secret key as in jwt_auth.js
-const secretKey = 'your_secret_key';
-
-// In-memory database for demonstration purposes (replace with a database in a real app).
-const users = [];
-
-// Middleware to check if a request has a valid JWT token.
-function authenticateToken(req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'Access denied' });
-
-    jwt.verifytoken, secretKey, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Invalid token' });
-        req.user = user;
-        next();
-    }
-}
-
-// Register a user
-app.post('/register', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-
-        // Check if the username already exists
-        if (users.find(user => user.username === username)) {
-            return res.status(400).json({ message: 'Username already exists' });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = { username, password: hashedPassword };
-        users.push(user);
-        res.status(201).json({ message: 'User registered' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Failed to register user' });
-    }
-});
-
-// Login and generate a JWT token
-app.post('/login', async (req, res) => {
-    const user = users.find(user => user.username === req.body.username);
-    if (user == null) return res.status(400).json({ message: 'User not found' });
-
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            const token = jwt.sign({ username: user.username }, secretKey);
-            res.json({ token });
-        } else {
-            res.status(401).json({ message: 'Invalid password' });
-        }
-    } catch {
-        res.status(500).json({ message: 'Login failed' });
-    }
-});
-
-// Protected route that requires a valid token
-app.get('/protected', authenticateToken, (req, res) => {
-    res.json({ message: 'This is a protected route', user: req.user });
-});
+app.use("/api/", router);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
--- routes.js
+++ routes.js
@@ -1,16 +1,25 @@
-import { Router } from 'express'
+import { Router } from 'express';
 import { helloWorldController } from './controllers/testController.js';
 import * as userController from './controllers/user/userController.js';
 import * as caregiverController from './controllers/caregiver/caregiverController.js';
 import * as orderController from './controllers/order/orderController.js';
 import * as reviewController from './controllers/review/reviewController.js';
 import * as serviceRatesController from './controllers/serviceRates/serviceRatesController.js';
+import { authenticateToken } from './controllers/auth/jwt_auth.js';
 
-const router  = Router();
+const router = Router();
+
 router.get('/', helloWorldController);
 
+// Apply the authenticateToken middleware to protected routes
+router.get('/user/:user_id/orders', authenticateToken, orderController.getAllOrdersByUserController);
+router.put('/order/:order_id', authenticateToken, orderController.updateOrderController);
+router.post('/order/:order_id/review', authenticateToken, reviewController.createReviewController);
+router.get('/service/:service_id/rates', authenticateToken, serviceRatesController.getServiceRatesController);
+router.post('/service/:service_id/rates', authenticateToken, serviceRatesController.createServiceRatesController);
+
+// Continue with your non-protected routes
 router.get('/user/:user_id', userController.getSingleUserController);
-router.get('/user/:user_id/orders', orderController.getAllOrdersByUserController);
 router.post('/user', userController.createUserController);
 
 router.get('/caregiver/:caregiver_id', caregiverController.getSingleCaregiverDetailController);
@@ -18,9 +27,5 @@ router.post('/caregiver/', caregiverController.createCaregiverController);
 
 router.get('/order/:order_id', orderController.getOneOrderController);
 router.post('/order', orderController.createOrderController);
-router.put('/order/:order_id', orderController.updateOrderController);
 
-router.post('/order/:order_id/review', reviewController.createReviewController);
-router.get('/service/:service_id/rates',serviceRatesController.getServiceRatesController);
-router.post('/service/:service_id/rates',serviceRatesController.createServiceRatesController);
 export default router;
