95ee0b3f012f6cf4589134112bde1a2636483c0c
Dewa Sinar Surya <55642480+Dewasurya16@users.noreply.github.com>
2023-11-10 18:03:41 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,12 +1,20 @@
-import { Router } from 'express'
+import { Router } from 'express';
 import { helloWorldController } from './controllers/testController.js';
 import * as userController from './controllers/user/userController.js';
 import * as caregiverController from './controllers/caregiver/caregiverController.js';
 import * as orderController from './controllers/order/orderController.js';
 import * as reviewController from './controllers/review/reviewController.js';
 import * as serviceRatesController from './controllers/serviceRates/serviceRatesController.js';
+import * as eventController from './controllers/Create Event/Create-Event_controler.js'; 
+import * as scheduleController from './controllers/GetSchedule/GetScheduleControler.js';
+import * as aggregateRatingController from './controllers/Agregate/AgregateControler.js'; 
+import * as getReviewsForOrderController from './controllers/Get-review/GetReviewControler.js'; 
 
-const router  = Router();
+const router = Router();
+const ratingController = require("./controllers/Agregate/AgregateControler.js");
+
+router.get("/order/:orderId/rating", ratingController.getAggregateRating);
+
 router.get('/', helloWorldController);
 
 router.get('/user/:user_id', userController.getSingleUserController);
@@ -21,6 +29,26 @@ router.post('/order', orderController.createOrderController);
 router.put('/order/:order_id', orderController.updateOrderController);
 
 router.post('/order/:order_id/review', reviewController.createReviewController);
-router.get('/service/:service_id/rates',serviceRatesController.getServiceRatesController);
-router.post('/service/:service_id/rates',serviceRatesController.createServiceRatesController);
+
+router.get('/service/:service_id/rates', serviceRatesController.getServiceRatesController);
+router.post('/service/:service_id/rates', serviceRatesController.createServiceRatesController);
+
+// Rute untuk fungsi cancel event
+router.put('/event/:event_id/cancel', eventController.cancelEventController);
+
+// Rute untuk fungsi create event
+router.post('/event', eventController.createEventController);
+
+// Rute untuk fungsi get review
+router.get('/order/:order_id/reviews', reviewController.getReviewsForOrderController);
+
+// Rute untuk fungsi get schedule
+router.get('/order/:order_id/schedule', scheduleController.getScheduleForOrderController);
+
+// Rute untuk fungsi Review List
+router.get('/reviews', reviewController.getReviewsForOrderController);
+
+// Rute untuk fungsi aggregate rating
+router.get('/order/:order_id/rating', aggregateRatingController.getAggregateRatingController);
+
 export default router;
