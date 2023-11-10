38bad9203894eeabb79199dc0baa54eb8b93d98b
Dewa Sinar Surya <55642480+Dewasurya16@users.noreply.github.com>
2023-11-10 17:47:21 +0700

    Merge function for agregate and GetReview

---
+++ repositories/AgregateRepository.js
@@ -0,0 +1,34 @@
+import { withPrisma } from "../middlewares/prismaMiddleware.js";
+
+export async function calculateAggregateRating(orderId) {
+  const order = await withPrisma(async (prisma) => {
+    return prisma.orders.findUnique({
+      where: {
+        order_id: orderId,
+      },
+      include: {
+        reviews: {
+          select: {
+            rating: true,
+          },
+        },
+      },
+    });
+  });
+
+  if (!order) {
+    throw new Error("Order not found");
+  }
+
+  const reviews = order.reviews;
+
+  if (!reviews || reviews.length === 0) {
+    return 0; // Jika tidak ada ulasan, rata-rata peringkat adalah 0.
+  }
+
+  // Menghitung rata-rata peringkat dari ulasan
+  const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+  const averageRating = totalRating / reviews.length;
+
+  return averageRating;
+}
