ab8c8bd96e37821ab8b6d2e437405dfafad158cb
Dewa Sinar Surya <55642480+Dewasurya16@users.noreply.github.com>
2023-11-11 21:45:08 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -5,11 +5,13 @@ import * as caregiverController from './controllers/caregiver/caregiverControlle
 import * as orderController from './controllers/order/orderController.js';
 import * as reviewController from './controllers/review/reviewController.js';
 import * as serviceRatesController from './controllers/serviceRates/serviceRatesController.js';
-import * as aggregateRatingController from './controllers/Agregate/AgregateControler.js'; 
+import * as eventController from './controllers/Create Event/Create-Event_controler.js'; 
+import * as scheduleController from './controllers/GetSchedule/GetScheduleControler.js';
+import * as aggregateRatingController from './repositories/AgregateRepository.js'; 
 import * as getReviewsForOrderController from './controllers/Get-review/GetReviewControler.js'; 
+import * as ratingController from "./controllers/Agregate/AgregateControler.js"; 
 
 const router = Router();
-const ratingController = require("./controllers/Agregate/AgregateControler.js");
 
 router.get("/order/:orderId/rating", ratingController.getAggregateRating);
 
@@ -31,12 +33,23 @@ router.post('/order/:order_id/review', reviewController.createReviewController);
 router.get('/service/:service_id/rates', serviceRatesController.getServiceRatesController);
 router.post('/service/:service_id/rates', serviceRatesController.createServiceRatesController);
 
+// Rute untuk fungsi cancel event
+router.put('/event/:event_id/cancel', eventController.cancelEventController); //done
+
+// Rute untuk fungsi create event
+router.post('/event', eventController.createEventController); // Done
 
 // Rute untuk fungsi get review
-router.get('/order/:order_id/reviews', reviewController.getReviewsForOrderController);
+router.get('/order/:order_id/reviews', getReviewsForOrderController.getReviewsForOrderController); // DONE
 
+// Rute untuk fungsi get schedule
+// router.get('/order/:order_id/schedule', scheduleController.getScheduleForOrderController); // Tidak ada tabel schedule
+
+// Rute untuk fungsi Review List
+router.get('/reviews', reviewController.getReviewsForOrderController2); // DONE
 
 // Rute untuk fungsi aggregate rating
-router.get('/order/:order_id/rating', aggregateRatingController.getAggregateRatingController);
+router.get('/order/:order_id/rating', aggregateRatingController.calculateAggregateRating); // DONE
 
 export default router;
+
