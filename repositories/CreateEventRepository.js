e53b95efdb0c916306fd36c6a0193e86b478eef5
Dewa Sinar Surya <55642480+Dewasurya16@users.noreply.github.com>
2023-11-11 21:33:27 +0700

    Update CreateEventRepository.js

--- repositories/CreateEventRepository.js
+++ repositories/CreateEventRepository.js
@@ -3,14 +3,29 @@ import { withPrisma } from "../middlewares/prismaMiddleware.js";
 export async function createEvent(eventData) {
   return withPrisma(async (prisma) => {
 
-    const { title, date, location, description } = eventData;
+    const { caregiver_id, start_date, end_date } = eventData;
 
     const newEvent = await prisma.events.create({
       data: {
-        title,
-        date: new Date(date).toISOString(),
-        location,
-        description,
+        caregiver_id,
+        start_date,
+        end_date,
+      },
+    });
+
+    return newEvent;
+  });
+}
+export async function cancelEvent(eventData) {
+  return withPrisma(async (prisma) => {
+
+
+    const newEvent = await prisma.events.update({
+      where:{
+        id:Number(eventData)
+      },
+      data: {
+        deleted_at: new Date().toISOString()
       },
     });
 
