ab33f083e49bf03d3fd2c8841b1c4f8f4d1118b7
FelixIG15 <felix.ganumba@gmail.com>
2023-11-13 01:11:20 +0700

    bugfix(controllers): removed low quality code of reviewControllers

--- controllers/review/reviewController.js
+++ controllers/review/reviewController.js
@@ -16,32 +16,3 @@ export const createReviewController = async (req, res) => {
     res.status(500).json({ error: `An error occurred ${error}` });
   }
 };
-export const getReviewsDetailForOrderController = async (req, res) => {
-  try {
-    const orderId = req.params.order_id;
-    const { review, review_rating } = req.body;
-
-    if (review_rating < 0 || review_rating > 5) {
-      res.status(500).json({ error: `An error occurred ${error}` });
-    }
-
-    const newReview = await createReview(orderId, review, review_rating);
-
-    res.json(newReview);
-  } catch (error) {
-    res.status(500).json({ error: `An error occurred ${error}` });
-  }
-};
-
-
-
-export const getReviewsForOrderController2 = async (req, res) => {
-  try {
-    const reviews = await withPrisma(async (prisma) => {
-      return prisma.reviews.findMany();
-    });
-    res.json(reviews);
-  } catch (error) {
-    res.status(500).json({ error: `An error occurred ${error}` });
-  }
-};
