4bdb59d196d3844226c24276d547bf639ec7dea9
FelixIG15 <felix.ganumba@gmail.com>
2023-11-13 00:41:52 +0700

    bugfix(app): fix event routes

--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ import caregiverRoutes from './routes/caregiverRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
 import serviceRatesRoutes from './routes/serviceRatesRoutes.js';
+import eventRoutes from './routes/eventRoutes.js';
 import { authenticateUser, checkAdmin } from './middlewares/authMiddleware.js';
 import * as adminController from './controllers/admin/adminController.js'; 
 
@@ -18,10 +19,8 @@ router.use('/caregiver', caregiverRoutes);
 router.use('/order', orderRoutes);
 router.use('/review', reviewRoutes);
 router.use('/service', serviceRatesRoutes);
+router.use('/event', eventRoutes);
 
 router.get('/admin/some-route', checkAdmin, adminController.someAdminRoute);
-
-router.put('/event/:event_id/cancel', eventController.cancelEventController);
-router.post('/event', eventController.createEventController);
 
 export default router;
